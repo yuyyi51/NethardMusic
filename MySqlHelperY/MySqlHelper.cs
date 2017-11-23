9b51dec81b14d027a8db3ac31e93e56c8d5d446c
迷途的指针 <yuyyi51@126.com>
2017-11-23 15:13:29 +0800

    细分了用户端API，完成了MySQL的操作辅助类，加入了数据库API用于和数据库交互。

---
+++ MySqlHelperY/MySqlHelper.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace MySqlHelperY
+{
+    public class MySqlHelper
+    {
+        protected MySqlConnection connection;
+
+        public MySqlHelper(string constr)
+        {
+            connection = new MySqlConnection(constr);
+        }
+
+        public async Task<DataSet> ExecuteDataSetAsync(string command)
+        {
+            DataSet data = new DataSet();
+            try
+            {
+                if (connection.State == ConnectionState.Closed)
+                    await connection.OpenAsync();
+                MySqlDataAdapter adapter = new MySqlDataAdapter(command, connection);
+                await adapter.FillAsync(data);
+                connection.Close();
+                return data;
+            }
+            catch(Exception e)
+            {
+                connection.Close();
+                throw e;
+            }
+        }
+
+        public async Task<int> ExecuteNonQueryAsync(string command)
+        {
+            try
+            {
+                if (connection.State == ConnectionState.Closed)
+                    await connection.OpenAsync();
+                MySqlCommand com = new MySqlCommand(command, connection);
+                return await com.ExecuteNonQueryAsync();
+            }
+            catch(Exception e)
+            {
+                connection.Close();
+                throw e;
+            }
+        }
+        public async Task<object> ExecuteScalar(string command)
+        {
+            try
+            {
+                if (connection.State == ConnectionState.Closed)
+                    await connection.OpenAsync();
+                MySqlCommand com = new MySqlCommand(command, connection);
+                return await com.ExecuteScalarAsync();
+            }
+            catch (Exception e)
+            {
+                connection.Close();
+                throw e;
+            }
+        }
+    }
+}
